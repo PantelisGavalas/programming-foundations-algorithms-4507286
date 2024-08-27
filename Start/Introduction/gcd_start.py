4a1c82376f6ff27a05314e00822cfb313a3247b7
PantelisGavalas <65115888+PantelisGavalas@users.noreply.github.com>
2024-08-27 09:25:14 +0000

    Created GCD Function.

--- Start/Introduction/gcd_start.py
+++ Start/Introduction/gcd_start.py
@@ -1,12 +1,11 @@
-# Example file for Programming Foundations: Algorithms by Joe Marini
-# Find the greatest common denominator of two numbers
-# using Euclid's algorithm
-
-
 def gcd(a, b):
-    pass
+    big = max(a, b)
+    small = min(a, b)
+    if big % small == 0:
+        return small
+    else:
+        return gcd(small, big % small)
 
 
-# try out the function with a few examples
-print(gcd(60, 96))  # should be 12
-print(gcd(20, 8))   # should be 4
+print(gcd(20, 8))
+print(gcd(60, 96))
